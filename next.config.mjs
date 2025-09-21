53ad7859767bebcc5950da7d082727b0e182db92
csrvoice <mycsrvoice@gmail.com>
2025-09-21 18:51:15 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dev.csrvoice.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
