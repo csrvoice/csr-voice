4f3e7f992cf2e3b571947795a9c0741315d86210
csrvoice <mycsrvoice@gmail.com>
2025-09-24 13:22:02 +0530

    gtag added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,49 +1,23 @@
 import "@/styles/globals.css";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Script from "next/script";
 
-// Your Google Analytics Measurement ID
-const GA_MEASUREMENT_ID = "G-YVPRSPFRB5";
-
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
-      });
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google Analytics Script */}
       <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YHZBWD1TL`}
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YHZBWD1TL');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
