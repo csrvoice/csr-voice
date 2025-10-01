d86b4d592865cd1a3acc532d84e751ecc4665384
csrvoice <mycsrvoice@gmail.com>
2025-10-01 13:00:34 +0530

    Add files via upload

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,38 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-YHZBWND1TL", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       {/* Google Analytics Script */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-YHZBWD1TL`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-YHZBWND1TL"
         strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="ga-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-YHZBWD1TL');
+          gtag('config', 'G-YHZBWND1TL', {
+            page_path: window.location.pathname,
+          });
         `}
       </Script>
 
